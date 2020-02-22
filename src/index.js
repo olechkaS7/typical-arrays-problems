5ed3e196f48323ab79269f5cadc8990b39440185
olechkaS7 <olg.iurist@yandex.ru>
2020-02-22 16:29:54 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,56 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (arguments[0] === undefined) {
+    return 0;
+  }
+
+  if (array.length === 0) {
+    return 0;
+  }
+
+  let min = array[0];
+  for (let i = 1; i < array.length; i += 1) {
+    if (array[i] < min) {
+      min = array[i];
+    }
+  }
+
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (arguments[0] === undefined) {
+    return 0;
+  }
+
+  if (array.length === 0) {
+    return 0;
+  }
+
+  let max = array[0];
+  for (let i = 1; i < array.length; i += 1) {
+    if (array[i] > max) {
+      max = array[i];
+    }
+  }
+
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (arguments[0] === undefined) {
+    return 0;
+  }
+
+  if (array.length === 0) {
+    return 0;
+  }
+
+  let sum = array[0];
+  for (let i = 1; i < array.length; i += 1) {
+    sum += array[i];
+  }
+
+  const avg = sum / array.length;
+  return avg;
 }
